d2d2eac6ae638c0cbcfb26a513620745ca55a1fb
Pedro Brandão <brandaopedrob@gmail.com>
2026-02-26 12:11:13 -0300

    alteração do nome da pasta e novos códigos de soma e soma com entradas

---
+++ primeiros-codigos/SomaComEntrada.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SomaComEntrada {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digite o primeiro número: ");
+        
+        int a = entrada.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+
+        int b = entrada.nextInt();
+
+        int soma = a + b;
+
+        System.out.println("A soma entre os dois números é: " + soma);
+
+        entrada.close();
+
+    }
+}
---
+++ primeiros-codigos/SomaSimples.java
@@ -0,0 +1,9 @@
+public class SomaSimples {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        int soma = a + b;
+
+        System.err.println("A soma é: " + soma);
+    }
+}
