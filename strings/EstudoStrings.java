6370c432ef1c12324ac25cc5d1507258d9ea0f3e
Pedro Brandão <brandaopedrob@gmail.com>
2026-03-17 17:41:47 -0300

    exercicios de string

---
+++ strings/EstudoStrings.java
@@ -0,0 +1,12 @@
+package strings;  // ou package strings.estudo;
+
+import java.util.Scanner;
+
+public class EstudoStrings {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Olá");
+        leitor.close();
+    }
+}
